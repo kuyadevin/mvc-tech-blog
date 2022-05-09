9ab2f4391237f8bc4130928e8a861acd9206ce03
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-08 17:28:21 -0700

    Install node modules connect-session in order to test if app is connecting to server, which it is

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
 const { route } = require('./blogRoutes');
 
-route.use('/users', userRoutes);
+router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
 
 module.exports = router;
