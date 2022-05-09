efc77b2fb414c2c8fcc2e13c34c2ea5b37d4b7ee
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-09 12:03:07 -0700

    Added a model for comments that can be made on a blof post as well as added a dashboard.handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
-const { route } = require('./blogRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
