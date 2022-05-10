06cfcd257345551d8358778bbf47e3aadec111eb
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-09 18:21:25 -0700

    Seperate login and sign up handlebars as well as event handlers

--- public/js/login.js
+++ public/js/login.js
@@ -16,25 +16,3 @@ const loginFormHandler = async (event) => {
         }
     }
 };
-
-const signUpFormHandler = async (event) => {
-    event.preventDefault();
-
-    const name = document.querySelector('#signUpName').value.trim();
-    const email = document.querySelector('#signUpEmail').value.trim();
-    const password = document.querySelector('#signUpPassword').value.trim();
-
-    if (name && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers : { 'Content-Type': 'application/json'},
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-        alert(res.statusText);
-        }
-    };
-}    
