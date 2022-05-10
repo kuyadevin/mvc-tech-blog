fc3c2c4764bb7b3570e06d584f522ac5667d3a91
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-09 17:47:48 -0700

    Addd part to render the edit blog page to dashboardRoutes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -88,7 +88,30 @@ router.get('/edit/:id', withAuth, async, (req, res) => {
             res.status(404).json({ message: 'No blog post found with this ID!'});
             return;
         }
-        res.status(200).json(editBlog);
+        const blog = editBlog.map((blog) => blog.get({plain: true}));
+        res.render('editBlog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Delete a blog post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deleteComment = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if (!deleteComment) {
+            res.status(404).json({ message: 'No comment found with this ID!'});
+            return;
+        }
+        res.status(200).json(deleteComment);
     } catch (err) {
         res.status(500).json(err);
     }
